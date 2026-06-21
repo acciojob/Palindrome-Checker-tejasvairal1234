c8b7f1bf9f44011eed7f0011e3b1b2a03ddf6689
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-06-21 16:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    let cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    // Compare with its reverse
+    return cleaned === cleaned.split('').reverse().join('');
 }
-module.exports = palindrome
+
+module.exports = palindrome;
